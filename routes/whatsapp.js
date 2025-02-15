fde0b0b30c22637fc48f265674dc02d05ad43bf6
Elcid-Chawa <elcidchawa@gmail.com>
2025-02-15 11:33:45 +0200

    added webhook verification

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -6,7 +6,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const WHATSAPP_API_URL = `https://graph.facebook.com/v22.0.0/${process.env.WHATSAPP_API_ID}/messages`;
-const ACCESS_TOKEN = process.env.WHATSAPP_API_SECRET;
+const ACCESS_TOKEN = process.env.GRAPH_API_TOKEN;
+const WEBHOOK_VERIFY_TOKEN = process.env.WHATSAPP_WEBHOOK_VERIFY_TOKEN;
 
 // Webhook for WhatsApp messages
 router.post("/webhook", async (req, res) => {
@@ -41,6 +42,24 @@ router.post("/webhook", async (req, res) => {
   );
 
   res.sendStatus(200);
+});
+
+// accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+router.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  // check the mode and token sent are correct
+  if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
+    // respond with 200 OK and challenge token from the request
+    res.status(200).send(challenge);
+    console.log("Webhook verified successfully!");
+  } else {
+    // respond with '403 Forbidden' if verify tokens do not match
+    res.sendStatus(403);
+  }
 });
 
 module.exports = router;
