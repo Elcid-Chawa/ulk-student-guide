396fa535798020f0b3cd2a589809548d51a7f165
Elcid-Chawa <elcidchawa@gmail.com>
2025-02-15 10:07:43 +0200

    fixed mongo db URI

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.MONGODB_URI, {
     serverSelectionTimeoutMS: 10000, // 10 seconds
     socketTimeoutMS: 45000, // 45 seconds
   })
